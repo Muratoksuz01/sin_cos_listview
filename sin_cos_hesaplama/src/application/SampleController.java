54686c59b0b4e7bccef40021f2ed11f71e7ddb84
Muratoksuz01 <muratoksuz208@gmail.com>
2024-04-18 12:21:22 +0300

    sin(x) +cos(y) hesaplayan ve sonucu listviewe kayıt etmek

---
+++ sin_cos_hesaplama/src/application/SampleController.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class SampleController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btn_hesapla;
+
+    @FXML
+    private Button btn_list_ekle;
+
+    @FXML
+    private Button btn_list_sil;
+
+    @FXML
+    private Button btn_temizle;
+
+    @FXML
+    private ListView<?> list_cos;
+
+    @FXML
+    private ListView<?> list_sin;
+
+    @FXML
+    private ListView<?> list_sonuc;
+
+    @FXML
+    void btn_hesapla_click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_list_ekle_click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_list_sil_click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_temzle_click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+      
+    }
+
+}
